57be263f2b3ad7e72e6ea9ce9d0503d9a9ec1a4a
smamidi15 <30820292+smamidi15@users.noreply.github.com>
2017-08-14 23:36:22 -0500

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ res.send('A request for subdivision three has been sent by Sreyansh and has arri
 // Do not change port, otherwise your app won't run on IMAD servers
 // Use 8080 only for local development if you already have apache running on 80
 
-var port - 80;
+var port=80;
 app.listen(port, function () {
   console.log(`IMAD course app listening on port ${port}!`);
 });
