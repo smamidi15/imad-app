0ee7cbc7090af41de23473917614e4b82c5027b1
smamidi15 <30820292+smamidi15@users.noreply.github.com>
2017-08-15 22:41:04 -0500

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,70 +5,7 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-var subdivisionOne = `
-    title:'Sreapp Subdivision One-Synthesized by Sreyansh Mamidi'
-    hyperlinks: 
-    `<div>
-            `a href="/">Home</a>
-            <a href="/subdivision-two">Sreapp Subdivision Two </a>
-            <a href="/subdivision-three">Sreapp Subdivison Three </a>
-            <hr/>
-        </div>
-    
-    <div>
-            <h3>Introduction</h3>
-            <h4> First Synthesized August 14th, 2017</h4>
-            <p>
-                Sreapp is an application designed to give the average human the knowledge of laudable paradoxes.
-            </p>
-        
-        </div>
-        <div>
-            <h1>The Grandfather Paradox</h1>
-            <p>
-                The Grandfather Paradox is very appealing to my personal sense of curiousity.
-            </p>
-        </div>;
-    style:' <style>
-            .info {
-                color:green;
-                font-family:courier;
-                Background:black;
-            }
-            
-            
-        </style>'
-`;
- function createTemplate (data)  {
-     var title= data.title;
-     var style= data.style;
-     var hyperlinks= data.hyperlinks;
-     var info= data.info;
-  
-var htmlTemplate = `
-<html>
-    <head>
-        <title>
-           ${title}
-        </title>
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <style>
-           ${style}
-        </style>
-    </head>
-    <body class=info>
-        <div>
-            ${hyperlinks}
-        </div>
-        <div>
-            ${info}
-        </div>
-    </body>
-</html>
-`;
 
-return htmlTemplate;
-}
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
@@ -82,7 +19,7 @@ app.get('/ui/madi.png', function (req, res) {
 });
 
 app.get('/subdivision-one', function (req, res) {
-     res.send(createTemplate(subdivisionOne));
+     res.sendFile(path.join(__dirname, 'ui', 'subdivision-one.html'));
 });
 
 app.get('/subdivision-two', function (req, res) {
